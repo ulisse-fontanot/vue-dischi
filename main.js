e1316e152948f4f6dec53048d3f65923dc203d5e
ulisse-fontanot <ulissef99@gmail.com>
2021-02-09 18:55:53 +0100

    quasi bonus

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 var app = new Vue({
   el: "#app",
   data:{
-    dischi: ""
+    dischi: "",
+    generi: []
   },
   mounted(){
     axios.get("https://flynn.boolean.careers/exercises/api/array/music").then(result => {
       this.dischi = result.data.response;
-    }).catch(error => console.log('errore'));
-    console.log(this.dischi);
+      console.log(this.dischi);
+      // -------------------
+      result.data.response.forEach((element) => {
+        if (!this.generi.includes(element.genre)) {
+          this.generi.push(element.genre);
+        }
+      })
+      console.log(this.generi);
+      // -------------------
+      this.generi.forEach((element) => {
+        document.getElementById('genere').innerHTML +=`
+        <option>${element}</option>
+        `
+      });
+      // -------------------
+      
+    })
+    .catch(error => console.log('errore'));
   }
 });
